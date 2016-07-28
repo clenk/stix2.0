e798e9a5f4f262f7914340e26355dbb9fbb1f883
John Wunder <jwunder@mitre.org>
2016-07-28 15:55:20 -0400

    I'm garbage at python but i think the validator works

--- validator/validator.py
+++ validator/validator.py
@@ -2,6 +2,11 @@ import glob, os, fnmatch, json
 from jsonschema import Draft4Validator, RefResolver
 from jsonschema import exceptions as schema_exceptions
 from urllib import urlopen
+import argparse
+
+parser = argparse.ArgumentParser(description='Validate STIX 2.0 against the schemas')
+parser.add_argument('docs', type=str, help="Documents to validate. Leave blank to validate everything in tests.", nargs='?')
+args = parser.parse_args()
 
 print ""
 
@@ -12,7 +17,9 @@ examples_dir = os.path.abspath(os.path.dirname(__file__) + '/../tests/')
 def default_handler (uri):
     return json.loads(urlopen("file://" + uri).read().decode("utf-8"))
 
-def run_test(schema, validator, test_case):
+def run_test(schema, test_case, schema_path):
+    validator = load_validator(schema_path, schema)
+
     with open(test_case) as instance_file:
         instance = json.load(instance_file)
 
@@ -20,44 +27,81 @@ def run_test(schema, validator, test_case):
     errors = sorted(validator.iter_errors(instance), key=lambda e: e.path)
 
     if len(errors) == 0:
-        print '.',
+        return True
     else:
-        error_messages = [(error.message) for error in errors]
-        all_errors.append({'file': test_case, 'errors': error_messages})
-        print 'E',
+        return [(error.message) for error in errors]
 
-# Essentially equivalent to "glob": load all JSON files in the examples directory and stick them in matches
-matches = []
-for root, dirnames, filenames in os.walk(examples_dir):
-    for filename in fnmatch.filter(filenames, '*.json'):
-        matches.append(os.path.join(root, filename))
+def run_tests():
+    # Essentially equivalent to "glob": load all JSON files in the examples directory and stick them in matches
+    matches = []
+    for root, dirnames, filenames in os.walk(examples_dir):
+        for filename in fnmatch.filter(filenames, '*.json'):
+            matches.append(os.path.join(root, filename))
 
-# Go through each test case, loading the appropriate schema based on directory hierarchy
-all_errors = []
-for test_case in matches:
-    schema_path = ('/').join(test_case.split('/tests/')[1].split('/')[0:-1]) + '.json'
-    with open(schemas_dir + '/' + schema_path) as schema_file:
-        cont = True
-        try:
+    # Go through each test case, loading the appropriate schema based on directory hierarchy
+    all_errors = []
+    for test_case in matches:
+        schema_path = ('/').join(test_case.split('/tests/')[1].split('/')[0:-1]) + '.json'
+        schema = load_schema(schemas_dir + '/' + schema_path)
+
+        results = run_test(schema, test_case, schemas_dir + '/' + schema_path)
+        if results == True:
+            print ".",
+        else:
+            print "E",
+            all_errors.append({'file': test_case, 'errors': results})
+
+    print_results(all_errors)
+    print "\n\n{0} passed, {1} errors\n".format(len(matches) - len(all_errors), len(all_errors))
+
+def load_schema(schema_path):
+    try:
+        with open(schema_path) as schema_file:
             schema = json.load(schema_file)
-        except ValueError as e:
-            all_errors.append({'file': test_case, 'errors': ['invalid JSON in schema or included schema: ' + schema_file.name + "\n" + str(e)]})
-            cont = False
+    except ValueError as e:
+        raise StixValidatorException('invalid JSON in schema or included schema: ' + schema_file.name + "\n" + str(e))
 
-        if cont:
-            try:
-                resolver = RefResolver(schemas_dir + '/' + schema_path, schema, {}, True, {"": default_handler})
-                validator = Draft4Validator(schema, resolver=resolver)
-                run_test(schema, validator, test_case)
-            except ValueError as e:
-                all_errors.append({'file': schema_path, 'errors': ['invalid JSON in instance: ' + test_case + "\n" + str(e)]})
-            except schema_exceptions.RefResolutionError:
-                all_errors.append({'file': test_case, 'errors': ['invalid JSON Schema']})
+    return schema
 
-# Print results
-for errors in all_errors:
-    print "\n\nFile: {0}".format(errors['file'])
-    print "----------------------------"
-    print ("\n").join(errors['errors'])
+def load_validator(schema_path, schema):
+    try:
+        resolver = RefResolver(schema_path, schema, {}, True, {"": default_handler})
+        validator = Draft4Validator(schema, resolver=resolver)
+    except schema_exceptions.RefResolutionError:
+        raise StixValidatorException('invalid JSON schema')
+    return validator
 
-print "\n\n{0} passed, {1} errors\n".format(len(matches) - len(all_errors), len(all_errors))
+def print_results(all_errors):
+    # Print results
+    for errors in all_errors:
+        print "\n\nFile: {0}".format(errors['file'])
+        print "----------------------------"
+        print ("\n").join(errors['errors'])
+
+class StixValidatorException(Exception):
+    def __init__(self,*args,**kwargs):
+        Exception.__init__(self,*args,**kwargs)
+
+if args.docs == None:
+    run_tests()
+else:
+    # Open the instance file
+    all_errors = []
+    with open(args.docs) as instance_file:
+        instance = json.load(instance_file)
+
+    # Load the schema corresponding to its type
+    schema_filename = instance['type']
+
+    matches = []
+    for root, dirnames, filenames in os.walk(schemas_dir):
+        for filename in fnmatch.filter(filenames, schema_filename + '.json'):
+            matches.append(os.path.join(root, filename))
+    if len(matches) > 0:
+        schema_path = matches[0]
+        schema = load_schema(schema_path)
+        results = run_test(schema, args.docs, schema_path)
+        if results == True:
+            print "Passed schema validation!"
+        else:
+            print_results([{'file': args.docs, 'errors': results}])
